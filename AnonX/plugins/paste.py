086679e52c531fa71770ae83fb02d3abaf9c5649
Arpit Singh <137784719+DAXXSIR@users.noreply.github.com>
2023-10-18 18:19:32 +0530

    Update paste.py

--- AnonX/plugins/paste.py
+++ AnonX/plugins/paste.py
@@ -1,72 +1,21 @@
-import asyncio
-import os
-import re
+import aiohttp
 
-import aiofiles
-from pykeyboard import InlineKeyboard
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton
-
-from AnonX import aiohttpsession as session
-from AnonX import pbot as app
-from AnonX.utils.errors import capture_err
-from AnonX.utils.pastebin import paste
-
-__mod_name__ = "Paste​"
-
-pattern = re.compile(
-    r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$"
-)
+BASE = "https://batbin.me/"
 
 
-async def isPreviewUp(preview: str) -> bool:
-    for _ in range(7):
-        try:
-            async with session.head(preview, timeout=2) as resp:
-                status = resp.status
-                size = resp.content_length
-        except asyncio.exceptions.TimeoutError:
-            return False
-        if status == 404 or (status == 200 and size == 0):
-            await asyncio.sleep(0.4)
-        else:
-            return True if status == 200 else False
-    return False
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
 
 
-@app.on_message(filters.command("paste") & ~filters.edited)
-@capture_err
-async def paste_func(_, message):
-    if not message.reply_to_message:
-        return await message.reply_text(
-            "Reply To A Message With /paste"
-        )
-    m = await message.reply_text("Pasting...")
-    if message.reply_to_message.text:
-        content = str(message.reply_to_message.text)
-    elif message.reply_to_message.document:
-        document = message.reply_to_message.document
-        if document.file_size > 1048576:
-            return await m.edit(
-                "You can only paste files smaller than 1MB."
-            )
-        if not pattern.search(document.mime_type):
-            return await m.edit("Only text files can be pasted.")
-        doc = await message.reply_to_message.download()
-        async with aiofiles.open(doc, mode="r") as f:
-            content = await f.read()
-        os.remove(doc)
-    link = await paste(content)
-    preview = link + "/preview.png"
-    button = InlineKeyboard(row_width=1)
-    button.add(InlineKeyboardButton(text="Paste Link", url=link))
-
-    if await isPreviewUp(preview):
-        try:
-            await message.reply_photo(
-                photo=preview, quote=False, reply_markup=button
-            )
-            return await m.delete()
-        except Exception:
-            pass
-    return await m.edit(link)
+async def Aarohibin(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
+    return link
